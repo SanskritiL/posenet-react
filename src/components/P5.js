9d614c381b385f2572d7b25fbc04c85199e5e5c2
Sanskriti Lamsal <slamsal@go.olemiss.edu>
2020-03-18 23:17:05 -0500

    useeffect minor changes

---
+++ src/components/P5.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Sketch from "react-p5"
+
+export default function P5(props){
+
+    return(
+        <h5>p5 js</h5>
+    )
+
+}
--- src/components/poseNet.js
+++ src/components/poseNet.js
@@ -1,64 +1,47 @@
 import React ,{useEffect,useRef,useState}from "react"
 import * as ml5 from 'ml5' //import ml5
-import Sketch from "react-p5"
+import P5 from "./P5"
 //collect the poses as required
 
 export default function PoseNet({webcamRef}){
 
-
+   
    let poseNet
-   let video
-   let x
-   let y
+   const [pose, setPose] = useState(false)
+  
 
    const loadPoseNet = () => {
-       poseNet = ml5.poseNet(webcamRef.current.video, modelLoaded) //initialize ml5.posenet
-   }
-
-
-   function modelLoaded() {
-       console.log("model loaded")
-   }
-   function gotPoses(poses){
-       if(poses.length > 0){
-         console.log(poses[0].pose)
-          
-          
-         
-       }
-   }
+       poseNet = ml5.poseNet(webcamRef.current.video, ()=>{ console.log("model loaded")}) //initialize ml5.posenet      
+       poseNet.on('pose', (poses)=> {
+        if(poses.length > 0){
+            setPose(poses[0].pose)
+           }
+    })
+    }
+  
    
-   const setup = (p5, webcamRef) => {
-    p5.createCanvas(680, 540).parent(webcamRef); // use parent to render canvas in this ref (without that p5 render this canvas outside your component)
-    video = p5.createCapture(p5.VIDEO)
-    video.hide();  
+       useEffect(()=>{
+        if(webcamRef) {
+            webcamRef.current.video.style.display = "none";
+          }
+        loadPoseNet(); 
+       },[])
+    
+       useEffect(()=>{
+           console.log(pose.keypoints)
+       },[pose])
+   
 
-};
-
- const draw = p5 => {
-       
-       p5.image(video, 0, 0)
-       p5.fill(255,0,0)
-       p5.ellipse(20, 20, 70, 70);
-   }
-
-    useEffect(()=>{
-         if(webcamRef){
-             webcamRef.current.video.style.display = "none";
-         }
-         loadPoseNet()
-         poseNet.on('pose', gotPoses)
-         
-
-    },[])
-
+    // useEffect(()=>{
+    //      getPose()
+    // },[pose])
  
     
 
     return(
         <div>
             <h1>{ml5.version}</h1>
-            <Sketch setup={setup} draw={draw}/>
+            <P5 pose={pose}/>
         </div>
     
     )
