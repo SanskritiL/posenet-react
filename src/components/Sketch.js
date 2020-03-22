1d154d26dafa8442fc2e3f2f4f5b67a9aba39d27
Sanskriti Lamsal <slamsal@go.olemiss.edu>
2020-03-22 10:48:23 -0500

    data collection wih brain

--- src/components/Sketch.js
+++ src/components/Sketch.js
@@ -3,9 +3,9 @@ export default function sketch(p) {
     let rotation = 0;
     let poses;
     let video;
-
     
-    console.log("hello")
+    
+    //console.log("here")
     p.setup = function () {
       p.createCanvas(1080, 840);
       video = p.createCapture(p.VIDEO);
@@ -17,12 +17,14 @@ export default function sketch(p) {
     
       if (props.poses !== null) {
         poses = props.poses
-        console.log("pose : ",poses)
+        //console.log("pose : ",poses)
       }
     };
   
     p.draw = function () {
        p.background(255,0,0)
+       p.translate(video.width, 0)
+       p.scale(-1,1)
        p.image(video, 0, 0,1080,840);
         if (poses) {
           poses.pose.keypoints.map(item => {
@@ -40,8 +42,6 @@ export default function sketch(p) {
             p.line(a.position.x, a.position.y, b.position.x, b.position.y)
          }
          
-      
-
         }
       
     };
--- src/components/poseNet.js
+++ src/components/poseNet.js
@@ -1,24 +1,56 @@
 import React, { useEffect, useRef, useState } from "react";
 import * as ml5 from "ml5"; //import ml5
 import P5 from "./P5";
+
 //collect the poses as required
+let brain;
+let poseNet;
+let x=1;
 
 export default function PoseNet({ webcamRef }) {
-  let poseNet;
+
+ 
+  let array;
+
   const [poses, setPoses] = useState(null);
-  
+  const [dataState, setDataState] = useState("waiting");
+  const [inputArray, setInputArray] = useState([]);
 
   const loadPoseNet = () => {
+    
     poseNet = ml5.poseNet(webcamRef.current.video, () => {
       console.log("model loaded");
     }); //initialize ml5.posenet
-    poseNet.on("pose", poses => {
-      if (poses.length > 0) {
-        setPoses(poses[0]);
-       
-      }
-    });
+
+    poseNet.on("pose", gotPoses);
+
+    let options = {
+      inputs: 34,
+      outputs: 4,
+      task: "classification",
+      debug: true
+    };
+    brain = ml5.neuralNetwork(options);
+    x = 5;
   };
+
+  const gotPoses = poses => {
+    if (poses.length > 0) {
+      setPoses(poses[0]);
+    }
+  }
+
+  //sets the data collection state
+  function collectDataMode() {
+    console.log("button pressed. you've got 10 secs");
+     setTimeout(() => {
+       setDataState("collecting");
+       setTimeout(() => {
+         setDataState("collected");
+       }, 10000);
+     }, 5000);
+   }
+
 
   useEffect(() => {
     if (webcamRef) {
@@ -27,17 +59,37 @@ export default function PoseNet({ webcamRef }) {
     loadPoseNet();
   }, []);
 
-  //    useEffect(()=>{
-  //        console.log("Pose is : ",pose)
-  //    },[pose])
 
-  // useEffect(()=>{
-  //      getPose()
-  // },[pose])
+
+  useEffect(() => {
+    if (dataState === "collecting") {
+      console.log("updated poses if collecting");
+      let tempInputArray = poses.pose.keypoints
+        .map(item => {
+          return [item.position.x, item.position.y];
+        })
+        .flat();
+     setInputArray(tempInputArray)
+    }
+  }, [poses]);
+
+  useEffect(() => {
+    console.log(x)
+    if (dataState === "collecting") {
+      brain.addData(inputArray, ["Y"])
+    };
+  }, [inputArray]);
+ 
+  const saveTrainingData = () => {
+    brain.saveData()
+  }
 
   return (
     <div>
       <h1>{ml5.version}</h1>
+      <h2>{dataState}</h2>
+      <button onClick={collectDataMode}> Collect Data </button>
+      <button onClick={saveTrainingData}> save Data </button>
       <P5 poses={poses} />
     </div>
   );
